43ebd5ac0694ff607694fe68362edf88ae11ef97
bellsilver7 <bellsilver7@naver.com>
2022-04-05 14:56:37 +0900

    type in typescript

--- index.ts
+++ index.ts
@@ -1,20 +1,7 @@
-const name = "bellsilver7",
-  age = 32,
-  gender = "male";
-
-const sayHi = (name, age, gender) => {
-  console.log(`Hello ${name}, you are ${age}, you are a ${gender}.`);
+const sayHi = (name: string, age: number, gender: string): string => {
+  return `Hello ${name}, you are ${age}, you are a ${gender}.`;
 };
 
-sayHi(name, age, gender);
-
-// sayHi(name, age); => 이렇게 사용하면 error
-
-// 선택적으로 사용하려면 ? 를 붙여준다.
-const sayHi2 = (name, age, gender?) => {
-  console.log(`Hello ${name}, you are ${age}, you are a ${gender}.`);
-};
-
-sayHi2(name, age);
+console.log(sayHi("bellsilver7", 32, "male"));
 
 export {};
