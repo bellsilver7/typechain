3a9ba792256d3620a61ad87d896727cbaafb3032
bellsilver7 <bellsilver7@naver.com>
2022-04-05 16:26:55 +0900

    Creating a Block part Three

--- src/index.ts
+++ src/index.ts
@@ -37,6 +37,7 @@ const genesisBlock: Block = new Block(
   "Hello",
   123456
 );
+
 // array of blocks!
 let blockchain: Block[] = [genesisBlock];
 
@@ -45,5 +46,28 @@ const getBlockchain = (): Block[] => blockchain;
 const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimstamp: number = getNewTimeStamp();
+  const newHash: string = Block.caclulateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimstamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimstamp
+  );
+
+  return newBlock;
+};
+
+console.log(createNewBlock("hello"), createNewBlock("bye bye"));
 
 export {};
