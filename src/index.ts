f12cdb27a69bd490969c897cf95fd993078cd3ab
bellsilver7 <bellsilver7@naver.com>
2022-04-05 15:51:45 +0900

    Classes on Typescript part One

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  private age: number;
+  public gender: string;
+
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "bellsilver7",
-  age: 32,
-  gender: "male",
-};
+const bellsilver7 = new Human("bellsilver7", 32, "male");
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}.`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(bellsilver7));
 
 export {};
